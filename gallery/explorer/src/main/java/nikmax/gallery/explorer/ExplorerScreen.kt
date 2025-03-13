3ab61afd9920d31b5e2891a58d15bd8bc1a49da0
NikMAX <null@>
2025-03-13 20:37:50 +0300

    Implemented collapsable topbar

--- gallery/explorer/src/main/java/nikmax/gallery/explorer/ExplorerScreen.kt
+++ gallery/explorer/src/main/java/nikmax/gallery/explorer/ExplorerScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -26,6 +27,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -73,6 +75,7 @@ private fun ExplorerScreenContent(
     onAction: (ExplorerVm.UserAction) -> Unit,
 ) {
     var showAppearanceSheet by remember { mutableStateOf(false) }
+    val topBarScrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     val focusManager = LocalFocusManager.current
 
     // if it's not a gallery root - display parent album content
@@ -116,6 +119,7 @@ private fun ExplorerScreenContent(
                                     }
                                 }
                             },
+                            scrollBehavior = topBarScrollBehavior,
                             focusManager = focusManager
                         )
                     }
@@ -159,6 +163,7 @@ private fun ExplorerScreenContent(
                             start = 8.dp,
                             end = 8.dp
                         )
+                        .nestedScroll(topBarScrollBehavior.nestedScrollConnection)
                 )
             }
 
