aaf4cc3e9bfbc6a60f0b353b622fecc59f1b8309
nikmax <null@null.null>
2025-04-04 22:00:19 +0300

    Made search topbar background transparent

--- gallery/explorer/src/main/java/nikmax/gallery/gallery/explorer/components/top_bars/SearchTopBar.kt
+++ gallery/explorer/src/main/java/nikmax/gallery/gallery/explorer/components/top_bars/SearchTopBar.kt
@@ -19,6 +19,7 @@ import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -27,6 +28,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -108,6 +110,9 @@ internal fun SearchTopBar(
             }
         },
         title = { /* has inner hardcoded paddings incompatible with searchbar */ },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors().copy(
+            containerColor = Color.Transparent
+        ),
         scrollBehavior = scrollBehavior
     )
 }
