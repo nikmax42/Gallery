d4174c99c1e9b9d42adf37aaa06f38ef024fba5b
nikmax <null@null.null>
2025-04-05 18:26:45 +0300

    Made MeasurementUnitsUtils.sizeToString result localizable

--- gallery/core/src/main/java/nikmax/gallery/gallery/core/utils/MeasurementUnitsUtils.kt
+++ gallery/core/src/main/java/nikmax/gallery/gallery/core/utils/MeasurementUnitsUtils.kt
@@ -1,15 +1,20 @@
 package nikmax.gallery.gallery.core.utils
 
-object MeasurementUnitsUtils {
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import nikmax.gallery.gallery.core.R
+import java.text.DecimalFormat
 
+object MeasurementUnitsUtils {
+    
     fun positionToPercentage(position: Long, duration: Long): Float {
         return position.toFloat() / duration.toFloat()
     }
-
+    
     fun percentageToPosition(percentage: Float, duration: Long): Long {
         return (percentage * duration).toLong()
     }
-
+    
     fun Long.videoDurationToString(): String {
         val totalSeconds = this / 1000
         val totalHours = totalSeconds / 3600
@@ -19,13 +24,26 @@ object MeasurementUnitsUtils {
         return if (totalHours > 0) "$hours:$minutes:$seconds"
         else "$minutes:$seconds"
     }
-
+    
+    @Composable
     fun Long.sizeToString(): String {
         return when {
-            this < 1024 -> "$this B"
-            this < 1024 * 1024 -> String.format("%.2f KB", this / 1024.0)
-            this < 1024 * 1024 * 1024 -> String.format("%.2f MB", this / (1024.0 * 1024.0))
-            else -> String.format("%.2f GB", this / (1024.0 * 1024.0 * 1024.0))
+            this < 1024 -> stringResource(R.string.b, this)
+            this < 1024 * 1024 -> stringResource(R.string.kb, this.kilobytes())
+            this < 1024 * 1024 * 1024 -> stringResource(R.string.mb, this.megabytes())
+            else -> stringResource(R.string.gb, this.gigabytes())
         }
+    }
+    
+    private fun Long.kilobytes(): String {
+        return DecimalFormat("#.00").format(this / 1024.0)
+    }
+    
+    private fun Long.megabytes(): String {
+        return DecimalFormat("#.00").format(this / (1024.0 * 1024.0))
+    }
+    
+    private fun Long.gigabytes(): String {
+        return DecimalFormat("#.00").format(this / (1024.0 * 1024.0 * 1024.0))
     }
 }
