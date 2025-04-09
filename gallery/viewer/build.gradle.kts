a6b92ffb4bc8108ed5913254676fac21f9e4545f
nikmax <null@null.null>
2025-04-09 21:47:57 +0300

    Added tests dependencies

--- gallery/viewer/build.gradle.kts
+++ gallery/viewer/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.turbine)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     // Coil
